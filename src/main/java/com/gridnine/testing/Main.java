a8906c5e02a19428f847ccd735eae7c0f0000197
Pavel-Fomchenkov <donotwritemail@gmail.com>
2024-06-27 21:31:36 +0300

    stream based

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -2,7 +2,6 @@ package com.gridnine.testing;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,82 +11,17 @@ public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
 
-        System.out.println("Все полеты.");
-        printFlights(flights);
-
         System.out.println("Без перелетов с вылетом до текущего момента времени.");
         printFlights(futureDepartureFlights(flights));
-        System.out.println("Повтор");
-        printFlights(fDF(flights));
-
 
         System.out.println("Без перелетов с прилетом раньше вылета.");
         printFlights(departureBeforeLandingFlights(flights));
-        System.out.println("Повтор");
-        printFlights(dBLF(flights));
 
-        System.out.println("Без перелетов с ожиданием более 2 часов.");
+        System.out.println("Без перелетов с ожиданием более " + hours + " часов.");
         printFlights(lessTimeWaitingFlights(flights, hours));
-        System.out.println("Повтор");
-        printFlights(lTWF(flights, hours));
     }
 
-    //    Вылет до текущего момента времени.
     private static List<Flight> futureDepartureFlights(List<Flight> flights) {
-        List<Flight> list = new ArrayList<>(flights);
-        for (int i = list.size() - 1; i >= 0; i--) {
-            for (Segment segment : list.get(i).getSegments()) {
-                if (segment.getDepartureDate().isBefore(LocalDateTime.now())) {
-                    list.remove(i);
-                    break;
-                }
-            }
-        }
-        return list;
-    }
-
-    //    Сегменты с датой прилёта раньше даты вылета.
-    private static List<Flight> departureBeforeLandingFlights(List<Flight> flights) {
-        List<Flight> list = new ArrayList<>(flights);
-        for (int i = list.size() - 1; i >= 0; i--) {
-            for (Segment segment : list.get(i).getSegments()) {
-                if (segment.getDepartureDate().isAfter(segment.getArrivalDate())) {
-                    list.remove(i);
-                    break;
-                }
-            }
-        }
-        return list;
-    }
-
-    //    Перелеты, где общее время, проведённое на земле, превышает два часа
-    //    (время на земле — это интервал между прилётом одного сегмента и вылетом следующего за ним).
-    private static List<Flight> lessTimeWaitingFlights(List<Flight> flights, int hours) {
-        List<Flight> list = new ArrayList<>(flights);
-        for (int i = list.size() - 1; i >= 0; i--) {
-            long totalDelay = 0L;
-            LocalDateTime arrival = null;
-            for (Segment segment : list.get(i).getSegments()) {
-                if (arrival != null) {
-                    totalDelay += ChronoUnit.HOURS.between(arrival, segment.getDepartureDate());
-                    if (totalDelay > (long) hours) {
-                        list.remove(i);
-                        break;
-                    }
-                }
-                arrival = segment.getArrivalDate();
-            }
-        }
-        return list;
-    }
-
-    private static void printFlights(List<Flight> flights) {
-        for (Flight flight : flights) {
-            System.out.println(flight);
-        }
-    }
-
-    private static List<Flight> fDF(List<Flight> flights) {
         return flights.stream()
                 .filter(flight ->
                         flight.getSegments().stream()
@@ -95,7 +29,7 @@ public class Main {
                 ).collect(Collectors.toList());
     }
 
-    private static List<Flight> dBLF(List<Flight> flights) {
+    private static List<Flight> departureBeforeLandingFlights(List<Flight> flights) {
         return flights.stream()
                 .filter(flight ->
                         flight.getSegments().stream()
@@ -103,7 +37,7 @@ public class Main {
                 ).collect(Collectors.toList());
     }
 
-    private static List<Flight> lTWF(List<Flight> flights, int hours) {
+    private static List<Flight> lessTimeWaitingFlights(List<Flight> flights, int hours) {
         return flights.stream()
                 .filter(flight -> {
                     long totalDelay = 0L;
@@ -121,4 +55,9 @@ public class Main {
                 }).collect(Collectors.toList());
     }
 
+    private static void printFlights(List<Flight> flights) {
+        for (Flight flight : flights) {
+            System.out.println(flight);
+        }
+    }
 }
